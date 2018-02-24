7a67f2898d30d685ad84693403f8b664f1296261
Usslessvoid <36482404+Usslessvoid@users.noreply.github.com>
2018-02-24 23:06:34 +0300

    q

---
+++ Lol3/src/Ohh.java
@@ -0,0 +1,13 @@
+
+public class Ohh {
+
+	public Ohh() {
+		// TODO јвтоматически созданна€ заглушка конструктора
+	}
+
+	public static void main(String[] args) {
+		// TODO јвтоматически созданна€ заглушка метода
+		System.out.println("Loll");
+	}
+
+}
